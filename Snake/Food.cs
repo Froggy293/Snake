509608bd392ca42c560fa9b07789c71b37301152
BuildTools <unconfigured@null.spigotmc.org>
2016-02-03 22:28:28 -0800

    https://github.com/Froggy293/Snake.git

---
+++ Snake/Food.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace Snake
+{
+    public class Food
+    {
+        public Rectangle Piece;
+        private int x, y, width = 10, height = 10;
+
+        public Food(Random rand)
+        {
+            Generate(rand);
+            Piece = new Rectangle(x, y, width, height);
+        }
+        public void Draw(Graphics graphics)
+        {
+            Piece.X = x;
+            Piece.Y = y;
+            graphics.FillRectangle(Brushes.Brown, Piece);
+        }
+        public void Generate(Random rand)
+        {
+            x = rand.Next(0, 70) * 10;
+            y = rand.Next(0, 60) * 10;
+        }
+    }
+}
---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Snake
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenu.Location = new System.Drawing.Point(0, 0);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(700, 600);
+            this.lblMenu.TabIndex = 0;
+            this.lblMenu.Text = "Snake\r\nCreated by Josh Laubach\r\n\r\nUse the arrow keys to change direction\r\nUse the" +
+    " space bar to pause\r\n\r\nPresss enter to begin.";
+            this.lblMenu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(700, 600);
+            this.Controls.Add(this.lblMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(716, 639);
+            this.Name = "formMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.formMain_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.formMain_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMenu;
+    }
+}
+
